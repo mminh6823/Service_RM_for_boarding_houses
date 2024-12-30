d2889bfa9579acf112c69d3c73cc056b335fa43f
mminh682003 <minhmacao1280@gmail.com>
2024-12-30 16:34:52 +0700

    Update_v8

--- obj/Debug/net8.0/Service_PhongTro.AssemblyInfo.cs
+++ obj/Debug/net8.0/Service_PhongTro.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Service_PhongTro")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+090191d5ac656ce84ec436638d9dc315c1bc7acc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+354e2b0e3869b67a409319569ebecd28a6b13c1e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Service_PhongTro")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Service_PhongTro")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
